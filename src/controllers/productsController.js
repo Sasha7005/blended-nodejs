8a3219dd909edd40b08da0debab1711a2a3bdcf9
Bas <basarab.o@icloud.com>
2026-02-22 15:36:03 +0200

    add task

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -14,3 +14,25 @@ export const getProductById = async (req, res) => {
   }
   res.status(200).json(product);
 };
+export const createProduct = async (req, res) => {
+  const product = await Product.create(product);
+  res.status(201).json(product);
+};
+export const updateProduct = async (req, res) => {
+  const { productId } = req.params;
+  const product = await Product.findByIdAndUpdate(productId, req.body, {
+    new: true,
+  });
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+  res.status(200).json(product);
+};
+export const deleteProduct = async (req, res) => {
+  const { productId } = req.params;
+  const product = await Product.findByIdAndDelete(productId);
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+  res.status(200).json(product);
+};
--- src/models/product.js
+++ src/models/product.js
@@ -3,9 +3,6 @@ import { Schema } from 'mongoose';
 
 const productSchema = new Schema(
   {
-    _id: {
-      type: String,
-    },
     name: {
       type: String,
       required: true,
