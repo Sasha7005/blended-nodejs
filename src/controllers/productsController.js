1c0f3ef33320341cbf59f408a7b5afca003b4ab8
Bas <basarab.o@icloud.com>
2026-02-24 13:19:37 +0200

    ed

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,58 +1,43 @@
 import createHttpError from 'http-errors';
-import productServices from '../services/productService.js';
+import { Product } from '../models/product.js';
 
-const getAllProducts = async (req, res) => {
-  const products = await productServices.getAllProducts();
-  return res.status(200).json({
-    data: products,
+export const getAllProducts = async (req, res) => {
+  const product = await Product.find();
+  return res.status(200).json(product);
+};
+
+export const getProductById = async (req, res) => {
+  const { productId } = req.params;
+  const product = await Product.findById(productId);
+
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+
+  res.status(200).json(product);
+};
+
+export const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  return res.status(201).json(product);
+};
+
+export const deleteProduct = async (req, res) => {
+  const { productId } = req.params;
+  const product = await Product.findByIdAndDelete(productId);
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+
+  res.status(200).json(product);
+};
+export const updateProduct = async (req, res) => {
+  const { productId } = req.params;
+  const product = await Product.findByIdAndUpdate(productId, req.body, {
+    new: true,
   });
-};
-
-const getProductById = async (req, res) => {
-  const { productId } = req.params;
-
-  const product = await productServices.getProductById(productId);
-
   if (!product) {
-    throw createHttpError(404, 'Product not found');
+    throw createHttpError(404, 'Note not found');
   }
-
-  res.status(200).json({ data: product });
-};
-
-const createProduct = async (req, res) => {
-  const product = await productServices.createProduct(req.body);
-
-  res.status(201).json({ data: product });
-};
-
-const updateProduct = async (req, res) => {
-  const { productId } = req.params;
-  const product = await productServices.updateProduct(productId, req.body);
-
-  if (!product) {
-    throw createHttpError(404, 'Product not found');
-  }
-
-  res.status(200).json({ data: product });
-};
-
-const deleteProduct = async (req, res) => {
-  const { productId } = req.params;
-
-  const product = await productServices.deleteProduct(productId);
-
-  if (!product) {
-    throw createHttpError(404, 'Product not found');
-  }
-
-  res.status(200).json({ data: product });
-};
-
-export {
-  getAllProducts,
-  getProductById,
-  createProduct,
-  updateProduct,
-  deleteProduct,
+  res.status(200).json(product);
 };
